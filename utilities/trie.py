2d94f7eb668ccf5fac406c72d47899ad228143a2
Tay Yang Shun <tay.yang.shun@gmail.com>
2017-06-03 14:33:55 +0800

    Add Python utility functions

--- utilities/trie.py
+++ utilities/trie.py
@@ -1,50 +1,50 @@
 class Trie(object):
-  def __init__(self):
-    """
-    Initialize your data structure here.
-    """
-    self.d = {}
+	def __init__(self):
+		"""
+		Initialize your data structure here.
+		"""
+		self.d = {}
 
-  def insert(self, word):
-    """
-    Inserts a word into the trie.
-    :type word: str
-    :rtype: void
-    """
-    curr = self.d
-    for char in word:
-      if char not in curr:
-        curr[char] = {}
-      curr = curr[char]
-    curr['#'] = True
+	def insert(self, word):
+		"""
+		Inserts a word into the trie.
+		:type word: str
+		:rtype: void
+		"""
+		curr = self.d
+		for char in word:
+			if char not in curr:
+				curr[char] = {}
+			curr = curr[char]
+		curr['#'] = True
 
-  def search(self, word):
-    """
-    Returns if the word is in the trie.
-    :type word: str
-    :rtype: bool
-    """
-    curr = self.d
-    for char in word:
-      if char in curr:
-        curr = curr[char]
-      else:
-        return False
-    return '#' in curr
+	def search(self, word):
+		"""
+		Returns if the word is in the trie.
+		:type word: str
+		:rtype: bool
+		"""
+		curr = self.d
+		for char in word:
+			if char in curr:
+				curr = curr[char]
+			else:
+				return False
+		return '#' in curr
 
-  def startsWith(self, prefix):
-    """
-    Returns if there is any word in the trie that starts with the given prefix.
-    :type prefix: str
-    :rtype: bool
-    """
-    curr = self.d
-    for char in prefix:
-      if char in curr:
-        curr = curr[char]
-      else:
-        return False
-    return True
+	def startsWith(self, prefix):
+		"""
+		Returns if there is any word in the trie that starts with the given prefix.
+		:type prefix: str
+		:rtype: bool
+		"""
+		curr = self.d
+		for char in prefix:
+			if char in curr:
+				curr = curr[char]
+			else:
+				return False
+		return True
 
 # Example
 word = 'hello'
